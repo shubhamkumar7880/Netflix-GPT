a620486fa3324bfeb076110fde1b1f3f4a7c9118
Shubham <shubhamkumar7880@gmail.com>
2023-12-29 11:34:54 +0530

    my changes

---
+++ src/components/Login.js
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+
+const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [errorMesaage, setErrorMessage] = useState(null);
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    if (isSignIn) {
+      const message = checkValidData(
+        email.current.value,
+        password.current.value,
+        "default"
+      );
+      setErrorMessage(message);
+    } else {
+      const message = checkValidData(
+        email.current.value,
+        password.current.value,
+        name.current.value
+      );
+      setErrorMessage(message);
+    }
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="bg img"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-4/12 absolute p-12 bg-black right-[30%] top-32 text-white rounded-lg bg-opacity-70"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full text-white bg-gray-700 rounded-lg"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email address"
+          className="p-4 my-4 w-full text-white bg-gray-700 rounded-lg"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full text-white bg-gray-700 rounded-lg"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMesaage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p
+          className="py-4 cursor-pointer"
+          onClick={() => setIsSignIn(!isSignIn)}
+        >
+          {isSignIn
+            ? "New to Netflix? Sign Up Now"
+            : "Already an account? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+export const checkValidData = (email, password, name) => {
+  const isEmailValid =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    );
+  const isPasswordValid =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+
+  if (!isEmailValid) return "Email Id is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+  if (name === "") return "Name is not valid";
+
+  return null;
+};
