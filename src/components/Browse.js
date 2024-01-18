bc5bc6387c89fd02b0a9d0898862f33c037e06b7
Shubham <shubhamkumar7880@gmail.com>
2024-01-18 17:17:20 +0530

    favicon added and improved protected routing

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "./hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
@@ -8,13 +8,21 @@ import useTopRatedMovies from "./hooks/useTopRated";
 import useUpcomingMovies from "./hooks/useUpcomingMovies";
 import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
+import FullPageLoader from "../utils/FullPageLoader";
+import { useNavigate } from "react-router-dom";
 
 const Browse = () => {
   const gptSearchView = useSelector((store) => store.gpt?.showGptSearch);
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) navigate("/");
+  }, []);
   useNowPlayingMovies();
   usePopularMovies();
   useTopRatedMovies();
   useUpcomingMovies();
+  if (!user) return <FullPageLoader />;
   return (
     <div>
       <Header />
